0cd536032e81cd8164cf4e7cffd4d57b39ad3cc7
tetsushi <tetsushi0501@gmail.com>
2018-10-26 15:06:18 +0900

    CodePushの処理を実装

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { CodePush } from '@ionic-native/code-push';
 
 @NgModule({
   declarations: [
@@ -24,6 +25,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    CodePush,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,80 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { CodePush } from '@ionic-native/code-push';
+
+// CodePushのキー
+const CODE_PUSH_IOS_KEY       = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const CODE_PUSH_ANDROID_KEY   = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor(public navCtrl: NavController) {
+  // 進捗を画面に表示するための変数
+  log = '';
 
+  constructor(
+    private platform: Platform,
+    private codePush: CodePush,
+  ) {}
+
+  ngOnInit() {
+    this.platform.ready().then(() => {
+      this.init();
+    });
   }
 
+  /**
+   * Ionicアプリが準備できた段階で呼ばれる初期化処理
+   */
+  private init() {
+    // アプリ再開を監視
+    this.platform.resume.subscribe(() => {
+      this.checkAndUpdate();
+    });
+
+    // アプリ起動時の同期
+    this.checkAndUpdate();
+  }
+
+  /**
+   * CodePushのアップデート
+   */
+  private async checkAndUpdate() {
+    this.putLog('CodePush.sync() 開始');
+
+    const deploymentKey = this.getDeploymentKey();
+    this.codePush.sync({
+      deploymentKey: deploymentKey,
+    }).subscribe(status => {
+      this.putLog(`SyncStatus = ${status}`);
+    }, error => {
+      this.putLog(`Error = ${error}`);
+    });
+  }
+
+  /**
+   * OSに応じたCodePushのキー
+   * @return CodePushのキー
+   */
+  private getDeploymentKey(): string {
+    if (this.platform.is('ios')) {
+      return CODE_PUSH_IOS_KEY;
+    } else if (this.platform.is('android')) {
+      return CODE_PUSH_ANDROID_KEY;
+    }
+
+    throw new Error('Not supported platform.');
+  }
+
+  /**
+   * OSに応じたCodePushのキー
+   * @param body 出力するログ
+   */
+  private putLog(body: string) {
+    this.log += `${body}<br>`;
+  }
 }
+
